336524e95ec1240d3d54c4d863a034876dbe1ddd
Dany Giguere <danygiguere23@gmail.com>
2025-04-30 16:14:34 -0400

    added home and users pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/en', pathMatch: 'full' },
+  {
+    path: 'en',
+    component: HomeComponent,
+    data: { lang: 'en' },
+  },
+  {
+    path: 'fr',
+    component: HomeComponent,
+    data: { lang: 'fr' },
+  },
+  {
+    path: 'en/users',
+    component: UsersComponent,
+    data: { lang: 'en' },
+  },
+  {
+    path: 'fr/users',
+    component: UsersComponent,
+    data: { lang: 'fr' },
+  },
+];
